f1fb49827a860c1694bf124b99281b7889cd3906
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-03-20 16:09:23 -0500

    Initial commit

---
+++ prophet.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from fbprophet import Prophet
+
+
+df = pd.read_csv('mexico.csv')
+df['y'] = np.log(df['y'])
+df.head()
+
+m = Prophet()
+m.fit(df)
+
+
+future = m.make_future_dataframe(periods=365)
+future.tail()
+
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+m.plot(forecast);
+m.plot_components(forecast);
